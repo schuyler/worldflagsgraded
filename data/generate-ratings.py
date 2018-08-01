a9bb27adb6508130b44d44ead7049734c8c6c691
Schuyler Erle <schuyler@nocat.net>
2018-08-01 09:38:55 -0700

    Generate ratings by score *or* by name

--- data/generate-ratings.py
+++ data/generate-ratings.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-import jinja2, json
+import jinja2, json, sys
 
+if len(sys.argv) < 2:
+    print("usage:", sys.argv[0], "[score|name]")
+    sys.exit(-1)
+
+key = sys.argv[1]
 tags = json.load(open("tags.json"))
 data = json.load(open("flags.json"))
 flags = list(data.values())
-flags.sort(key=lambda a: a["score"])
+flags.sort(key=lambda a: a[key])
 flags.reverse()
 
 tmpl = jinja2.Template(open("ratings.tmpl").read())
