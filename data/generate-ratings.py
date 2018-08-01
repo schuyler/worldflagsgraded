7b233ad286620b070123cf17118180ed24b69b2f
Schuyler Erle <schuyler@nocat.net>
2018-08-01 10:21:08 -0700

    Regen new designs with path option

--- data/generate-ratings.py
+++ data/generate-ratings.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 
-import jinja2, json, sys
+import jinja2, json, sys, os.path
 
 if len(sys.argv) < 2:
     print("usage:", sys.argv[0], "[score|name]")
     sys.exit(-1)
 
 key = sys.argv[1]
-tags = json.load(open("tags.json"))
-data = json.load(open("flags.json"))
+path = sys.argv[2] if len(sys.argv) > 2 else ""
+base = os.path.dirname(sys.argv[0]) or "."
+
+tags = json.load(open(base + "/tags.json"))
+data = json.load(open(base + "/flags.json"))
 flags = list(data.values())
 flags.sort(key=lambda a: a[key])
 flags.reverse()
 
 tmpl = jinja2.Template(open("ratings.tmpl").read())
-print(tmpl.render(flags=flags, tags=tags))
+print(tmpl.render(flags=flags, tags=tags, path=path))
